08211715c7499cd1b0ea6832370bb67df9a5af37
Matúš Mrázik <matus.mrazik2@gmail.com>
2017-03-05 17:21:43 +0100

    created basic ui

--- SEM01/src/MainWindow.hpp
+++ SEM01/src/MainWindow.hpp
@@ -12,11 +12,11 @@ class MainWindow : public QMainWindow
 	Q_OBJECT
 
 public:
-	explicit MainWindow(QWidget *parent = 0);
+	explicit MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
 private:
-	Ui::MainWindow *ui;
+	Ui::MainWindow *_ui;
 };
 
 #endif // MAINWINDOW_HPP
